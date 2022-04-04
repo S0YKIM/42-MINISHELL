eafc5c251b643adfeedd1c10ec6db64a06f31b31
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-04 21:43:05 +0900

    Add header file (#3)

---
+++ includes/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/04 20:05:42 by sokim             #+#    #+#             */
+/*   Updated: 2022/04/04 21:02:36 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/errno.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
