bb3ec96db7b7f92d551443b4c00d5df7463fa925
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-08 20:33:54 +0900

    Add ft_isdigit (#3)

--- libft/libft.h
+++ libft/libft.h
@@ -6,5 +6,6 @@ size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strdup(const char *str);
 void	*ft_memset(void *b, int c, size_t len);
+int		ft_isdigit(int c);
 
 #endif
