e556b191cd6439d32d591c96c30b3f3ad4c5f4d1
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-13 02:40:32 +0900

    Add printing error message when there are more than one arg (#6)

--- srcs/builtins/ft_env.c
+++ srcs/builtins/ft_env.c
@@ -6,16 +6,21 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 18:53:17 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/12 21:42:26 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/13 02:39:00 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_env(t_data *data)
+int	ft_env(char **cmds, t_data *data)
 {
 	t_env	*curr;
 
+	if (cmds[1])
+	{
+		printf("env: %s: No such file or directory\n", cmds[1]);
+		return (127);
+	}
 	curr = data->env_list;
 	while (curr)
 	{
