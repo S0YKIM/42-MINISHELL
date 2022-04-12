cb56c97966839a7cf989c410e7c3d1356cba005b
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-13 02:15:09 +0900

    Add function remove_node() (#23)

---
+++ srcs/env/remove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   remove.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 02:00:18 by sokim             #+#    #+#             */
+/*   Updated: 2022/04/13 02:10:16 by sokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	remove_node(t_env *node)
+{
+	if (node->prev)
+		node->prev->next = node->next;
+	if (node->next)
+		node->next->prev = node->prev;
+	free(node->key);
+	free(node->value);
+	free(node);
+}
