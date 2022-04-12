21c90c80902355666711d60f9e8aa1a1e451daab
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-13 01:40:17 +0900

    Refactor with dividing into three cases: 1. No node exists 2. Node with the key name exists 3. There are nodes but no node matches with the key name (#6)

--- includes/env.h
+++ includes/env.h
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 13:34:53 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/13 00:14:24 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/13 00:34:07 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 #include "struct.h"
 
-t_env	*add_new_env_node(char *key, char *value);
 int		update_env(t_data *data, char *key, char *value);
 t_env	*get_node_with_key(t_data *data, char *key);
 char	*get_env_value_with_key(t_data *data, char *key);
--- srcs/env/update.c
+++ srcs/env/update.c
@@ -6,13 +6,13 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 13:08:27 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/12 22:19:17 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/13 01:36:24 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_env	*add_new_env_node(char *key, char *value)
+static t_env	*add_new_env_node(char *key, char *value)
 {
 	t_env	*new;
 
@@ -22,33 +22,44 @@ t_env	*add_new_env_node(char *key, char *value)
 	new->key = ft_strdup(key);
 	new->value = ft_strdup(value);
 	new->next = NULL;
-	free(key);
-	free(value);
 	return (new);
+}
+
+static int	change_value(t_env **curr, char *key, char *value)
+{
+	char	*tmp;
+
+	while (*curr)
+	{
+		if (!ft_strncmp((*curr)->key, key, ft_strlen(key)))
+		{
+			tmp = (*curr)->value;
+			(*curr)->value = ft_strdup(value);
+			free(tmp);
+			return (SUCCESS);
+		}
+		if (!(*curr)->next)
+			break ;
+		*curr = (*curr)->next;
+	}
+	return (FAILURE);
 }
 
 int	update_env(t_data *data, char *key, char *value)
 {
 	t_env	*curr;
-	char	*tmp;
 
 	curr = data->env_list;
-	while (curr)
-	{
-		if (!ft_strncmp(curr->key, key, ft_strlen(key)))
-		{
-			tmp = curr->value;
-			curr->value = ft_strdup(value);
-			free(tmp);
-			break ;
-		}
-		curr = curr->next;
-	}
 	if (!curr)
 	{
-		curr = add_new_env_node(key, value);
-		if (!curr)
+		data->env_list = add_new_env_node(key, value);
+		if (!data->env_list)
 			return (FAILURE);
 	}
+	else if (change_value(&curr, key, value) == SUCCESS)
+		return (SUCCESS);
+	else
+		if (!(curr->next = add_new_env_node(key, value)))
+			return (FAILURE);
 	return (SUCCESS);
 }
