769b3ca49c54f984f0397bccac392b2bd3ebb7b1
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-13 00:15:06 +0900

    Add function to find node with the key name (#23)

--- includes/env.h
+++ includes/env.h
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 13:34:53 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/12 22:23:42 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/13 00:14:24 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 t_env	*add_new_env_node(char *key, char *value);
 int		update_env(t_data *data, char *key, char *value);
+t_env	*get_node_with_key(t_data *data, char *key);
 char	*get_env_value_with_key(t_data *data, char *key);
 
 #endif
