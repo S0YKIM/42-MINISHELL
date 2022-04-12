df31204721088fea1cbd5f2f927bd18e50ddfe2c
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-12 22:26:44 +0900

    Rename function to get_env_value_with_key (#23)

--- includes/env.h
+++ includes/env.h
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 13:34:53 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/10 13:43:40 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/12 22:23:42 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 #include "struct.h"
 
+t_env	*add_new_env_node(char *key, char *value);
 int		update_env(t_data *data, char *key, char *value);
-char	*get_env_value(t_data *data, char *key);
+char	*get_env_value_with_key(t_data *data, char *key);
 
 #endif
