1678915fe4c3d5d5b52ad0c98cbbf98f8bb3044c
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-13 17:40:40 +0900

    Rename function to create_new_env_node (#23)

--- srcs/env/update.c
+++ srcs/env/update.c
@@ -6,13 +6,13 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 13:08:27 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/13 01:49:17 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/13 17:31:07 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_env	*add_new_env_node(char *key, char *value, t_env *prev)
+static t_env	*create_new_env_node(char *key, char *value, t_env *prev)
 {
 	t_env	*new;
 
@@ -53,14 +53,17 @@ int	update_env(t_data *data, char *key, char *value)
 	curr = data->env_list;
 	if (!curr)
 	{
-		data->env_list = add_new_env_node(key, value, NULL);
+		data->env_list = create_new_env_node(key, value, NULL);
 		if (!data->env_list)
 			return (FAILURE);
 	}
 	else if (change_value(&curr, key, value) == SUCCESS)
 		return (SUCCESS);
 	else
-		if (!(curr->next = add_new_env_node(key, value, curr)))
+	{
+		curr->next = create_new_env_node(key, value, curr);
+		if (!curr->next)
 			return (FAILURE);
+	}
 	return (SUCCESS);
 }
