4b07dcaf2ba94a3b956e3c0a898fe0c7b84ed9c0
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-15 16:06:39 +0900

    Remove ft_strncmp()

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 12:27:14 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/15 15:44:27 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/15 16:05:20 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	while (str1[i] != str2[i] || !str1[i] ||| !str2[i])
+	while (str1[i] != str2[i] || !str1[i] || !str2[i])
 		++i;
 	return (str1[i] - str2[i]);
 }
