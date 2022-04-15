28a53d6652730076c4bef5444da96403b8128493
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-15 21:33:07 +0900

    Add function to check if string is in the range of long long

--- srcs/builtins/utils.c
+++ srcs/builtins/utils.c
@@ -6,11 +6,61 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 23:38:55 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/13 19:15:32 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/15 21:27:43 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	is_str_num(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+static int	is_space(int c)
+{
+	if (c == ' ' || (9 <= c && c <= 13))
+		return (1);
+	return (0);
+}
+
+int	is_str_long_long(char *str)
+{
+	long long	num;
+	long long	tmp;
+	int			sign;
+
+	num = 0;
+	sign = 1;
+	while (is_space(*str))
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (ft_isdigit(*str))
+	{
+		tmp = num;
+		num = num * 10 + (*str - '0');
+		if (num < tmp)
+			return (FALSE);
+		str++;
+	}
+	return (TRUE);
+}
 
 int	is_valid_key_name(char *key)
 {
