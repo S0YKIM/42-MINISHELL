d3f02987469dab745d0a5542780d74b76d63d8ee
hhkim0729 <heehkim@student.42seoul.kr>
2022-04-17 14:37:10 +0900

    Fix expanding env error (#7)

--- srcs/parsing/expand.c
+++ srcs/parsing/expand.c
@@ -6,29 +6,34 @@
 /*   By: heehkim <heehkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 22:36:12 by heehkim           #+#    #+#             */
-/*   Updated: 2022/04/17 14:11:11 by heehkim          ###   ########.fr       */
+/*   Updated: 2022/04/17 14:33:44 by heehkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*find_env_key(char *i, char **key_end)
+static char	*expand_env_value_exception(char *i, char **key_end)
+{
+	char	*tmp;
+
+	if (!**key_end)
+		return (ft_strdup("$"));
+	tmp = ft_strchr(*key_end, '\"');
+	if (tmp)
+		*key_end = tmp - 1;
+	return (ft_substr(i, 0, *key_end - i + 1));
+}
+
+static char	*expand_env_value(t_data *data, char *i, char **key_end)
 {
 	char	*key_start;
 	char	*key;
-	char	*tmp;
+	char	*value;
 
 	key_start = i + 1;
 	*key_end = key_start;
-	if (!(**key_end))
-		return (ft_strdup("$"));
-	if (ft_strchr("$\'\"", **key_end))
-	{
-		tmp = ft_strchr(*key_end, '\"');
-		if (tmp)
-			*key_end = tmp - 1;
-		return (ft_substr(i, 0, *key_end - i + 1));
-	}
+	if (!(**key_end) || ft_strchr("$\'\"", **key_end))
+		return (expand_env_value_exception(i, key_end));
 	while (**key_end)
 	{
 		if (!*(*key_end + 1) || ft_strchr("$\'\"", *(*key_end + 1)))
@@ -36,47 +41,51 @@ static char	*find_env_key(char *i, char **key_end)
 		(*key_end)++;
 	}
 	key = ft_substr(key_start, 0, *key_end - key_start + 1);
-	return (key);
-}
-
-static char	*expand_new_token(t_data *data, t_token *curr, char *i, int *len)
-{
-	char	*key;
-	char	*value;
-	char	*key_end;
-	char	*head;
-	char	*new_token;
-
-	key = find_env_key(i, &key_end);
 	if (!key)
 		return (NULL);
 	value = get_env_value(data, key);
 	free(key);
+	return (value);
+}
+
+static int	expand_new_token(t_data *data, t_token *curr, char *i)
+{
+	char	*value;
+	char	*key_end;
+	char	*head;
+	int		len;
+	char	*new_token;
+
+	value = expand_env_value(data, i, &key_end);
 	if (!value)
-		return (NULL);
+		return (ERROR);
 	*i = '\0';
 	head = ft_strjoin(curr->data, value);
 	free(value);
 	if (!head)
-		return (NULL);
-	*len = ft_strlen(head);
+		return (ERROR);
+	len = ft_strlen(head);
 	new_token = ft_strjoin(head, key_end + 1);
 	free(head);
-	return (new_token);
-}
-
-static int	expand_and_replace(t_data *data, t_token *curr, char *i)
-{
-	int		len;
-	char	*new_token;
-
-	new_token = expand_new_token(data, curr, i, &len);
 	if (!new_token)
 		return (ERROR);
 	free(curr->data);
 	curr->data = new_token;
 	return (len);
 }
+
+// static int	expand_and_replace(t_data *data, t_token *curr, char *i)
+// {
+// 	int		len;
+// 	char	*new_token;
+
+// 	new_token = expand_new_token(data, curr, i, &len);
+// 	if (!new_token)
+// 		return (ERROR);
+// 	free(curr->data);
+// 	curr->data = new_token;
+// 	return (len);
+// }
 
 static int	expand_env_quote(t_data *data, t_token *curr, char **i)
 {
@@ -92,7 +101,7 @@ static int	expand_env_quote(t_data *data, t_token *curr, char **i)
 		dollar = ft_strchr(*i + 1, '$');
 		if (dollar && dollar < end)
 		{
-			len = expand_and_replace(data, curr, dollar);
+			len = expand_new_token(data, curr, dollar);
 			if (len == ERROR)
 				return (FALSE);
 			*i = curr->data + len + 1;
@@ -116,7 +125,7 @@ int	expand_env(t_data *data, t_token *curr)
 	{
 		if (*i == '$')
 		{
-			len = expand_and_replace(data, curr, i);
+			len = expand_new_token(data, curr, i);
 			if (len == ERROR)
 				return (FALSE);
 			i = curr->data + len;
