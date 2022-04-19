be86121fd3ba17bd43be2b9b2309034e8701d446
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-19 22:29:02 +0900

    Add builtins comments

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: heehkim <heehkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 16:41:06 by heehkim           #+#    #+#             */
-/*   Updated: 2022/04/19 14:37:36 by heehkim          ###   ########.fr       */
+/*   Updated: 2022/04/19 22:26:33 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	main(int argc, char **argv, char **envp)
 		return (FAILURE);
 	init_data(&data);
 	g_env_list = NULL;
+	set_signal();
 	if (!parse_env(envp))
 		exit(EXIT_FAILURE);
 	while (TRUE)
@@ -35,19 +36,19 @@ int	main(int argc, char **argv, char **envp)
 		if (!create_astree(&data))
 			exit(EXIT_FAILURE);
 		// if (!ft_strcmp(data.astree->right->argv[0], "cd"))
-		// 	ft_cd(data.astree->right, &data);
+		// 	ft_cd(data.astree->right);
 		// else if (!ft_strcmp(data.astree->right->argv[0], "echo"))
 		// 	ft_echo(data.astree->right);
 		// else if (!ft_strcmp(data.astree->right->argv[0], "env"))
-		// 	ft_env(data.astree->right, &data);
+		// 	ft_env(data.astree->right);
 		// else if (!ft_strcmp(data.astree->right->argv[0], "exit"))
-		// 	ft_exit(data.astree->right, &data);
+		// 	ft_exit(data.astree->right);
 		// else if (!ft_strcmp(data.astree->right->argv[0], "export"))
-		// 	ft_export(data.astree->right, &data);
+		// 	ft_export(data.astree->right);
 		// else if (!ft_strcmp(data.astree->right->argv[0], "pwd"))
 		// 	ft_pwd();
 		// else if (!ft_strcmp(data.astree->right->argv[0], "unset"))
-		// 	ft_unset(data.astree->right, &data);
+		// 	ft_unset(data.astree->right);
 		if (!traverse_heredoc(data.astree))
 			exit(EXIT_FAILURE);
 		free(line);
