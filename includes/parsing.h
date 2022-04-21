e0f42886d33af8f5dcd49992a08770cb2ab3d3aa
hhkim0729 <heehkim@student.42seoul.kr>
2022-04-21 23:22:58 +0900

    Add counting pl to ast_simplify (#102)

--- includes/parsing.h
+++ includes/parsing.h
@@ -6,7 +6,7 @@
 /*   By: heehkim <heehkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 19:17:43 by heehkim           #+#    #+#             */
-/*   Updated: 2022/04/19 15:57:12 by heehkim          ###   ########.fr       */
+/*   Updated: 2022/04/21 23:02:18 by heehkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		expand_env(t_token *curr);
 int		create_astree(t_data *data);
 t_ast	*create_ast_node(t_token *curr);
 t_ast	*add_ast_node(t_ast *parent, t_ast *new);
-int		simplify_astree(t_ast *node);
+int		simplify_astree(t_data *data, t_ast *node);
 
 // 나중에 삭제
 void	display_env_list(void);
