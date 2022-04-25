2b880e646d9bb661816dab790ec45a02abad4379
hhkim0729 <heehkim@student.42seoul.kr>
2022-04-25 16:57:31 +0900

    Refactor signal.c (#3 #13)

--- includes/signal.h
+++ includes/signal.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: heehkim <heehkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 13:57:17 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/19 13:58:11 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/25 16:56:29 by heehkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 # define SIGNAL_H
 
 void	set_signal(void);
+
+void	handle_signal(int signum);
 
 #endif
