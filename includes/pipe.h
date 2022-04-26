8ac8029ffb733b7522b146995b26f0a645e2cc2e
hhkim0729 <heehkim@student.42seoul.kr>
2022-04-27 01:40:34 +0900

    Refactor pipe/redirection.c (#3)

--- includes/pipe.h
+++ includes/pipe.h
@@ -6,7 +6,7 @@
 /*   By: heehkim <heehkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 15:50:33 by heehkim           #+#    #+#             */
-/*   Updated: 2022/04/27 01:36:00 by heehkim          ###   ########.fr       */
+/*   Updated: 2022/04/27 01:39:16 by heehkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define PIPE_H
 
 int		traverse_heredoc(t_ast *ast);
-void	traverse_redirection(t_ast *ast, int *in_fd, int *out_fd);
+int		traverse_redirection(t_ast *ast, int *in_fd, int *out_fd);
 int		fork_process(t_data *data);
 
 int		execute(t_data *data);
