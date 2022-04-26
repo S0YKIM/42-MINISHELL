0995dc0f8493456f1f54bbc57d0d81a86d889911
hhkim0729 <heehkim@student.42seoul.kr>
2022-04-26 18:06:21 +0900

    Add initiating envp to init_data (#164)

--- srcs/util/init.c
+++ srcs/util/init.c
@@ -6,20 +6,21 @@
 /*   By: heehkim <heehkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 16:53:49 by heehkim           #+#    #+#             */
-/*   Updated: 2022/04/24 14:24:01 by heehkim          ###   ########.fr       */
+/*   Updated: 2022/04/26 17:28:55 by heehkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	init_data(t_data *data)
+static void	init_data(t_data *data, char **envp)
 {
 	ft_memset(data, 0, sizeof(t_data));
+	data->envp = envp;
 }
 
 int	init(t_data *data, char **envp)
 {
-	init_data(data);
+	init_data(data, envp);
 	g_env_list = NULL;
 	set_signal();
 	if (!parse_env(envp))
