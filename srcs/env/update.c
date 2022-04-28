fb500d4b1fe465c742f3dbd5b3a15c4523c5c3c1
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-28 15:37:29 +0900

    Refactor update_env() to place environment variables in alphabetical order (#23)

--- srcs/env/update.c
+++ srcs/env/update.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 13:08:27 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/15 16:59:02 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/28 15:35:52 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ static int	add_new_env_node(t_data *data, char *key, char *value)
 	new = (t_env *)ft_calloc(1, sizeof(t_env));
 	if (!new)
 		return (FALSE);
-	new->key = ft_strdup(key);
-	new->value = ft_strdup(value);
+	new->key = key;
+	new->value = value;
 	if (!data->env_list)
 		data->env_list = new;
 	else
@@ -35,31 +35,47 @@ static int	add_new_env_node(t_data *data, char *key, char *value)
 	return (TRUE);
 }
 
-static int	change_value(t_data *data, char *key, char *value)
+static int	insert_new_env_node(t_env *curr, char *key, char *value)
 {
-	t_env	*curr;
+	t_env	*new;
+
+	new = (t_env *)ft_calloc(1, sizeof(t_env));
+	if (!new)
+		return (FALSE);
+	new->key = key;
+	new->value = value;
+	new->prev = curr->prev;
+	new->next = curr;
+	if (curr->prev)
+		curr->prev->next = new;
+	curr->prev = new;
+	return (TRUE);
+}
+
+static int	change_value(t_env *curr, char *value)
+{
 	char	*tmp;
 
-	curr = data->env_list;
-	while (curr)
-	{
-		if (!ft_strcmp(curr->key, key))
-		{
-			tmp = curr->value;
-			curr->value = ft_strdup(value);
-			free(tmp);
-			return (TRUE);
-		}
-		curr = curr->next;
-	}
-	return (FALSE);
+	if (!value)
+		return (TRUE);
+	tmp = curr->value;
+	curr->value = value;
+	free(tmp);
+	return (TRUE);
 }
 
 int	update_env(t_data *data, char *key, char *value)
 {
-	if (change_value(data, key, value) == TRUE)
-		return (TRUE);
-	if (add_new_env_node(data, key, value) == FALSE)
-		return (FALSE);
-	return (TRUE);
+	t_env	*curr;
+
+	curr = data->env_list;
+	while (curr)
+	{
+		if (!ft_strcmp(key, curr->key))
+			return (change_value(curr, value));
+		else if (ft_strcmp(key, curr->key) < 0)
+			return (insert_new_env_node(curr, key, value));
+		curr = curr->next;
+	}
+	return (add_new_env_node(data, key, value));
 }
