688da921ffae53ecfc518147c1f9151a3dd462cb
Soyeon Kim <soyeonkim.9683@gmail.com>
2022-04-30 21:32:45 +0900

    Fix error in ft_exit() by returning exit code instead of exit (#6)

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: sokim <sokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 19:52:52 by sokim             #+#    #+#             */
-/*   Updated: 2022/04/24 21:44:39 by sokim            ###   ########.fr       */
+/*   Updated: 2022/04/30 21:30:12 by sokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,42 +26,45 @@ static void	print_numeric_arg_required(char *argv)
 	ft_putendl_fd(": numeric argument required", STDERR_FILENO);
 }
 
-static void	exit_code_reserved(char *code, char *argv, int cnt)
+static long long	exit_code_reserved(char *code, char *argv)
 {
 	long long	num;
 
 	num = ft_atol(code);
-	if (cnt == 1)
+	if (num == 0)
+	{
 		printf("exit\n");
+		exit(num);
+	}
 	if (num == 1)
 		print_too_many_arg();
 	else if (num == 255)
 		print_numeric_arg_required(argv);
-	exit(num);
+	return (num);
 }
 
-static void	exit_code_custom(char *code, int cnt)
+static void	exit_code_custom(char *code)
 {
 	long long	num;
 
-	if (cnt == 1)
-		printf("exit\n");
+	printf("exit\n");
 	num = ft_atol(code);
 	exit(num);
 }
 
-void	ft_exit(t_ast *ast, t_data *data)
+int	ft_exit(t_ast *ast)
 {
 	if (ast->argc == 1)
-		exit_code_reserved("0", NULL, data->pl_cnt);
+		return (exit_code_reserved("0", NULL));
 	else if (ast->argc == 2)
 	{
-		if (!is_str_num(ast->argv[1]))
-			exit_code_reserved("255", ast->argv[1], data->pl_cnt);
-		if (!is_str_long_long(ast->argv[1]))
-			exit_code_reserved("255", ast->argv[1], data->pl_cnt);
-		exit_code_custom(ast->argv[1], data->pl_cnt);
+		if (!is_str_num(ast->argv[1]) \
+			|| !is_str_long_long(ast->argv[1]))
+			return (exit_code_reserved("255", ast->argv[1]));
+		else
+			exit_code_custom(ast->argv[1]);
 	}
 	else
-		exit_code_reserved("1", NULL, data->pl_cnt);
+		return (exit_code_reserved("1", NULL));
+	return (0);
 }
